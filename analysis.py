df8f8397c378a003dc16394a12e19a51e9fcc353
James Wright <james@chumsley.org>
2015-09-24 20:29:56 -0700

    initial impl of code for db backend and analysis

---
+++ analysis.py
@@ -0,0 +1,63 @@
+import sys
+import csv
+from warnings import warn
+from numpy import std, sqrt, log
+from scipy.stats import t as tdist
+from bogota.db import mle_param, MissingData, index_str
+from bogota.tasks import fit_fold
+
+def csv_fig(fname, rows, report_intermediate=False, **commonKwArgs):
+    missing = MissingData()
+    with open(fname, 'wt') as s:
+        f = csv.writer(s, delimiter='\t')
+        for row in rows:
+            csvrow = []
+            for cell in row:
+                args = dict(commonKwArgs)
+                args.update(cell)
+                pool_obj = eval(args['pool_name'], sys.modules)
+                unif_ll = pool_obj.uniform_log_likelihood() / log(10.0) / args['num_folds']
+                (avg, err) = mle_parameter_interval(**args)
+                csvrow.append('%.8f' % ((avg/log(10.0))-unif_ll))
+                csvrow.append('%.4f' % (err/log(10.0)))
+            f.writerow(csvrow)
+    return missing
+
+def mle_parameter_interval(parameter_name,
+                           solver_name, pool_name, fold_seeds, num_folds,
+                           by_game, stratified,
+                           p_val=0.05, queue_missing=False):
+    missing_count = 0
+    avgs = []
+    for fold_seed in fold_seeds:
+        data = []
+        for fold_idx in xrange(max(num_folds, 1)):
+            try:
+                data.append(mle_param(parameter_name,
+                                      solver_name, pool_name, fold_seed, num_folds, fold_idx,
+                                      by_game, stratified))
+            except MissingData:
+                missing_count += 1
+                if queue_missing:
+                    fit_fold(solver_name, pool_name, fold_seed, num_folds, fold_idx,
+                             by_game, stratified)
+        if len(data) > 0:
+            avgs.append(sum(data)/len(data))
+
+    if missing_count > 0:
+        warn("%d/%d rows missing for %s" % (missing_count, max(len(fold_seeds)*num_folds, 1),
+                                            index_str(solver_name, pool_name, fold_seeds, num_folds, "", by_game, stratified)))
+    if len(avgs) > 0:
+        return tdist_confidence_interval(avgs, p_val)
+
+def tdist_confidence_interval(avgs, p_val):
+    """
+    Return ``(a,e)``, where ``a`` is the average of ``avgs``, and ``e`` is the
+    magnitude of an error bar for a ``(1-p_val)`` confidence interval.
+    """
+    n = float(len(avgs))
+    assert n > 1
+    avg = sum(avgs) / n
+    q = 1.0 - (p_val / 2.0)
+    width = tdist.ppf(q, n-1) / sqrt(n)
+    return (avg, width*std(avgs))
---
+++ db.py
@@ -0,0 +1,212 @@
+"""
+Interface for saving/restoring values from the database.
+"""
+import sys
+
+# =================================== Config ==================================
+try:
+    import local
+except ImportError:
+    class LocalConfig(object):
+        pass
+    local = LocalConfig()
+    local.DB_TYPE = 'sqlite3'
+    local.DB_NAME = 'bogota_results.db'
+    local.DB_HOST = None
+    local.DB_PORT = None
+    local.DB_USER = None
+    local.DB_PASS = None
+
+# =================================== Saving ==================================
+
+def save_mle_param(parameter_name, parameter_value, restart_idx,
+                   solver_name, pool_name, fold_seed, num_folds, fold_idx,
+                   by_game, stratified):
+    with db_connect() as db:
+        # TODO
+        pass
+
+# ================================== Loading ==================================
+
+def mle_param(parameter_name,
+              solver_name, pool_name, fold_seed, num_folds, fold_idx,
+              by_game, stratified):
+    """
+    Return the MLE estimate of ``parameter_name`` for the specified model and
+    data fold combination.
+    """
+    with db_connect() as db:
+        jobid = _ensure_jobid(db, solver_name, pool_name, fold_seed, num_folds, fold_idx, by_game, stratified)
+        c = db.cursor()
+
+        # Choose parameter value associated with the highest training LL
+        c.execute(_sql(db,
+                       """
+                       select p.value, l.value
+                       from mle_parameters p
+                       join mle_parameters l on l.restart_idx = p.restart_idx and l.jobid = p.jobid
+                       where p.jobid = %s
+                       and p.name = %s
+                       and l.name = 'TRAIN_LL'
+                       order by l.value desc
+                       limit 1
+                       """), [jobid, parameter_name])
+        vals = c.fetchall()
+        c.close()
+        if len(vals) == 0:
+            raise MissingData(solver_name, pool_name, fold_seed, num_folds, fold_idx, by_game, stratified)
+    return vals[0][0]
+
+# =================================== Utils ===================================
+
+def index_str(solver_name, pool_name, fold_seed, num_folds, fold_idx,
+              by_game, stratified):
+    return "%s/%s/%s/%s/%s/%s/%s" % (solver_name, pool_name, fold_seed, num_folds, fold_idx, by_game, stratified)
+
+class MissingData(Exception):
+    def __init__(self, solver_name, pool_name, fold_seed, num_folds, fold_idx,
+                 by_game, stratified):
+        self.solver_name = solver_name
+        self.pool_name = pool_name
+        self.fold_seed = fold_seed
+        self.num_folds = num_folds
+        self.fold_idx = fold_idx
+        self.by_game = by_game
+        self.stratified = stratified
+
+    def __repr__(self):
+        return "%s.%s(%s, %s, %s, %s, %s, %s, %s)" % (self.__class__.__module__, self.__class__.__name__,
+                                                      repr(self.solver_name), repr(self.pool_name), repr(self.fold_seed), repr(self.num_folds), repr(self.fold_idx), repr(self.by_game), repr(self.stratified))
+    def __str__(self):
+        return "<MissingData %s>" % index_str(self.solver_name, self.pool_name, self.fold_seed, self.num_folds, self.fold_idx, self.by_game, self.stratified)
+
+def _ensure_jobid(self, db, solver_name, pool_name, fold_seed, num_folds, fold_idx, by_game, stratified, recursive=False):
+    """
+    Load the jobid out of the database, creating a new job record if necessary.
+    """
+    c = db.cursor()
+    sql = _sql(db, 'select jobid from mle_jobs'
+                   ' where solver_name=%s and pool_name=%s and fold_seed=%s and num_folds=%s and fold_idx=%s and by_game=%s and stratified=%s')
+    c.execute(sql, [solver_name, pool_name, fold_seed, num_folds, fold_idx, by_game, stratified])
+    jobids = c.fetchall()
+    if len(jobids) > 1:
+        raise ValueError('Multiple jobids for %s/%s/%s/%s/%s/%s/%s: %s' % \
+                         (solver_name, pool_name,
+                          fold_seed, num_folds, fold_idx, by_game, stratified,
+                          jobids))
+    elif len(jobids) == 1:
+        self.jobid = jobids[0][0]
+
+    elif recursive:
+        raise IOError("Could not create jobid for %s/%s/%s/%s/%s/%s/%s" % \
+                         (solver_name, pool_name,
+                          fold_seed, num_folds, fold_idx, by_game, stratified))
+
+    sql = _sql(db, 'insert into mle_jobs (solver_name, pool_name, fold_seed, num_folds, fold_idx, by_game, stratified) '
+                   'values (%s,%s,%s,%s,%s, %s,%s)')
+    c.execute(sql, [solver_name, pool_name, fold_seed, num_folds, fold_idx, by_game, stratified])
+    db.commit()
+    return _ensure_jobid(db, solver_name, pool_name, fold_seed, num_folds, fold_idx, by_game, stratified, recursive=True)
+
+def db_connect(db_type, db_name, db_host=None, db_port=None, db_user=None, db_pass=None):
+    if db_type == 'sqlite3':
+        import sqlite3
+        db = sqlite3.connect(db_name)
+    elif db_type == 'mysql':
+        import mysql.connector
+        db = mysql.connector.connect(user=db_user, password=db_pass, db=db, host=db_host, port=db_port)
+    else:
+        raise ValueError("Unknown db_type '%s'" % db_type)
+    return db
+
+def create_schema(db):
+    """
+    Create all expected tables in ``db``.
+    """
+    c = db.cursor()
+    sql = """
+    create table mle_jobs (
+    jobid integer primary key %s,
+    solver_name varchar(256) not null,
+    pool_name varchar(128) not null,
+    fold_seed integer not null,
+    num_folds integer not null,
+    fold_idx integer not null,
+    by_game boolean not null default false,
+    stratified boolean not null default false
+    )
+    """
+    if db.__class__.__module__ == 'mysql.connector.connection':
+        sql = (sql % 'auto_increment')
+        sql += 'ENGINE=InnoDB'
+    else:
+        sql = (sql % 'autoincrement')
+    c.execute(sql)
+
+    sql = """
+    create index reverse_index on mle_jobs
+    (solver_name, pool_name, fold_seed, num_folds, fold_idx)
+    """
+    c.execute(sql)
+
+    sql = _sql(db, """
+    create table mle_parameters (
+    jobid integer not null,
+    restart_idx integer not null,
+    name varchar(64) not null,
+    value double not null,
+    primary key (jobid, restart_idx, name))
+    """)
+    if db.__class__.__module__ == 'mysql.connector':
+        sql += 'ENGINE=InnoDB'
+    c.execute(sql)
+
+    sql = _sql(db, """
+    create table mle_checkpoints (
+    jobid integer not null,
+    restart_idx integer not null,
+    rng_state blob(3000) not null,
+    primary key (jobid))
+    """)
+    if db.__class__.__module__ == 'mysql.connector':
+        sql += 'ENGINE=InnoDB'
+    c.execute(sql)
+
+    db.commit()
+
+
+def _sql(db, str):
+    """
+    Convert ``str`` to use qmark style if necessary for ``db``.
+    """
+    mod = sys.modules[db.__class__.__module__]
+    mod = sys.modules[mod.__package__]
+    if mod.paramstyle == 'qmark':
+        return str.replace('%s', '?')
+    else:
+        return str
+
+def _binary(db, value):
+    """
+    Wrap ``value`` in the Binary accessor provided by ``db``'s module.
+    """
+    mod = sys.modules[db.__class__.__module__]
+    mod = sys.modules[mod.__package__]
+    return mod.Binary(value)
+
+def _solver(spec):
+    """
+    Evaluate ``spec`` if necessary to construct a Solver.
+    """
+    if isinstance(spec, str) or isinstance(spec, unicode):
+        # strip "broken" prefix
+        if spec[0:len("broken.")] == "broken.":
+            print "stripping 'broken.' from '%s'" % (spec,)
+            spec = spec[len("broken."):]
+        if spec[-1] == ')':
+            return eval(spec, sys.modules)
+        else:
+            return eval(spec + '()', sys.modules)
+    else:
+        # Passthrough
+        return spec
