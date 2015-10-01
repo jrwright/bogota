d342298e62093944e18351ceee08026d284ab667
James Wright <james@chumsley.org>
2015-10-01 12:22:34 -0700

    make async optional

--- celeryapp.py
+++ celeryapp.py
@@ -1,19 +1,29 @@
 from __future__ import absolute_import
-from celery import Celery
-import bogota.cdf as cfg
+import bogota.cfg as cfg
 
-app = Celery(cfg.app.name,
-             broker=cfg.app.broker,
-             backend=cfg.app.backend,
-             include=cfg.app.include.split(','))
+if cfg.app.async:
+    from celery import Celery
 
-# Optional configuration, see the application user guide.
-app.conf.update(
-    CELERY_TASK_RESULT_EXPIRES=3600,
-    CELERY_TASK_SERIALIZER='json',
-    CELERY_RESULT_SERIALIZER='json',
-    CELERY_ACCEPT_CONTENT = ['json'],
-)
+    app = Celery(cfg.app.name,
+                 broker=cfg.app.broker,
+                 backend=cfg.app.backend,
+                 include=cfg.app.include.split(','))
 
-if __name__ == '__main__':
-    app.worker_main()
+    # Optional configuration, see the application user guide.
+    app.conf.update(
+        CELERY_TASK_RESULT_EXPIRES=3600,
+        CELERY_TASK_SERIALIZER='json',
+        CELERY_RESULT_SERIALIZER='json',
+        CELERY_ACCEPT_CONTENT = ['json'],
+    )
+
+    if __name__ == '__main__':
+        app.worker_main()
+
+else:
+    class DummyApp(object):
+        def task(fn, *args, **kwArgs):
+            return fn
+
+    app = DummyApp()
+
