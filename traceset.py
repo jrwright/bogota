e9e1d69128474de2c79512912a3d4f44aec63889
James Wright <james@chumsley.org>
2015-10-20 10:45:57 -0700

    impl train_fold_gamewise

--- traceset.py
+++ traceset.py
@@ -14,13 +14,9 @@ class TraceSet(object):
     def __getitem__(self, idx):
         return self.traceset[idx]
 
-    def train_fold(self, fold_seed, num_folds, fold_idx, return_both=False):
-        """
-        Return the training fold for the specified fold_seed/num_folds/fold_idx.
-        If 'return_both' is True, return the test fold as well.
-        """
+    def _divide_folds(self, items, fold_seed, num_folds, fold_idx):
         r = np.random.RandomState(fold_seed)
-        N = len(self.traceset)
+        N = len(items)
         n = int(N) / int(num_folds)
         boundary = int(N) % int(num_folds)
         if fold_idx < boundary:
@@ -30,11 +26,52 @@ class TraceSet(object):
             s = N - (n * (num_folds - fold_idx))
 
         indices = r.permutation(N)
-        train = list(self.traceset)
+        train = list(items)
         test = list([])
         for i in indices[s:s+n]:
-            test.append(self.traceset[i])
-            train.remove(self.traceset[i])
+            test.append(items[i])
+            train.remove(items[i])
+
+        return train,test
+
+    def train_fold(self, fold_seed, num_folds, fold_idx, return_both=False, stratified=False):
+        """
+        Return the training fold for the specified fold_seed/num_folds/fold_idx.
+        If 'return_both' is True, return the test fold as well.
+        """
+        assert stratified == False
+        train, test = self._divide_folds(self.traceset, fold_seed, num_folds, fold_idx)
+
+        if return_both:
+            return TraceSet(train), TraceSet(test)
+        else:
+            return TraceSet(train)
+
+    def train_fold_gamewise(self, fold_seed, num_folds, fold_idx, return_both=False, stratified=False):
+        """
+        Divide by both subject and game.  The test set will contain neither games
+        nor subjects from the training set.
+        """
+        assert stratified == False
+
+        # Collect all the games
+        gset = set()
+        for tr in self.traceset:
+            for ai in tr:
+                gset.add(ai.player.game)
+        gset = sorted(gset, key=repr)
+        train_games, test_games = self._divide_folds(gset, fold_seed, num_folds, fold_idx)
+
+        # Break every trace into a training portion and a testing portion
+        train = []
+        test = []
+        for tr in self.traceset:
+            trn_tr = [ai for ai in tr if ai.player.game in train_games]
+            if len(trn_tr) > 0:
+                train.append(trn_tr)
+            tst_tr = [ai for ai in tr if ai.player.game in test_games]
+            if len(tst_tr) > 0:
+                test.append(tst_tr)
 
         if return_both:
             return TraceSet(train), TraceSet(test)
