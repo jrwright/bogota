8b10353e489a78e8a4c87ef95ecd90af0cbc2197
James Wright <james@chumsley.org>
2015-10-28 10:40:31 -0700

    impl noisy introspection and external gambit models

---
+++ ext.py
@@ -0,0 +1,98 @@
+"""
+Solver support for external solvers (QRE and Nash equilibrium).
+"""
+from __future__ import absolute_import
+from numpy import inf
+from gambit.qre import ExternalStrategicQREPathTracer
+from gambit.nash import ExternalEnumPureSolver, ExternalLCPSolver, ExternalGlobalNewtonSolver
+from .solver import solver
+from .utils import proportionally_mix_profiles
+
+@solver(fittable_parameters=['lam'],
+        parameter_bounds={'lam':(0.0, None)})
+def qre(game, lam):
+    """
+    Return a quantal response equilibrium with precision 'lam' for 'game'.
+    """
+    s = ExternalStrategicQREPathTracer()
+    return s.compute_at_lambda(game, lam)[0]
+
+@solver(fittable_parameters= ['eps'],
+        parameter_bounds={'eps':(0.0, 1.0)})
+def nash(game, eps):
+    """
+    Return a Nash equilibrium mixed with uniform noise.
+    """
+    s = ExternalEnumPureSolver()
+    eqa = s.solve(game)
+    assert len(eqa) == 1, "%d eqa found" % len(eqa)
+    return proportionally_mix_profiles([eps, 1.0-eps],
+                                       [game.mixed_profile(), eqa[0]])
+
+NASH_CACHE = {}
+
+@solver(fittable_parameters= ['eps'],
+        parameter_bounds={'eps':(0.0, 1.0)})
+def nee(game, eps):
+    """
+    Return a set of Nash equilibria, each mixed with uniform noise.
+    """
+    global NASH_CACHE
+    if game in NASH_CACHE:
+        eqa = NASH_CACHE[game]
+    elif len(game.players) == 2:
+        s = ExternalLCPSolver()
+        eqa = s.solve(game)
+        NASH_CACHE[game] = eqa
+    else:
+        s = ExternalGlobalNewtonSolver()
+        eqa = s.solve(game)
+        NASH_CACHE[game] = eqa
+    assert len(eqa) > 0
+    return [proportionally_mix_profiles([eps, 1.0-eps], [game.mixed_profile(), eqm]) for eqm in eqa]
+
+@solver(fittable_parameters= ['eps'],
+        parameter_bounds={'eps':(0.0, 1.0)})
+def bnee(game, eps, prediction_cache=None):
+    """
+    For each `eps`-noised equilibrium of `game`, find the one that performs best
+    on the `peek_data` entry of `prediction_cache` and return it.
+    """
+    eqa = nee(game, eps)
+    if len(eqa) == 1:
+        return eqa[0]
+
+    wp = prediction_cache['peek_data']
+    best_ll = -inf
+    best_eqm = None
+    for eqm in eqa:
+        ll = wp.log_likelihood(eqm)
+        if ll > best_ll or best_eqm is None:
+            best_eqm = eqm
+            best_ll = ll
+    return best_eqm
+
+@solver(fittable_parameters= ['eps'],
+        parameter_bounds={'eps':(0.0, 1.0)})
+def wnee(game, eps, prediction_cache=None):
+    """
+    For each `eps`-noised equilibrium of `game`, find the one that performs worst
+    on the `peek_data` entry of `prediction_cache` and return it.
+    """
+    eqa = nee(game, eps)
+    if len(eqa) == 1:
+        return eqa[0]
+    elif len(eqa) == 0:
+        raise RuntimeError("OH NOE WHERE IS EQM")
+
+    wp = prediction_cache['peek_data']
+    assert wp.game == game
+    worst_ll = inf
+    worst_eqm = None
+    for eqm in eqa:
+        ll = wp.log_likelihood(eqm)
+        if ll < worst_ll or worst_eqm is None:
+            worst_eqm = eqm
+            worst_ll = ll
+    return worst_eqm
+
---
+++ noisy_introspection.py
@@ -0,0 +1,52 @@
+"""
+Implement the noisy introspection model from [Goeree and Holt 2004].
+"""
+from __future__ import absolute_import
+from numpy.linalg import norm
+from .solver import solver
+from .cognitive_hierarchy import logit_br_all
+
+@solver(fittable_parameters=['lam0', 'tel'],
+        parameter_bounds={'lam0':(0.0,None), 'tel':(1.05, None)})
+def noisy_introspection(game, lam0, tel, thresh=1e-6, return_steps=False, max_steps=1000):
+    """
+    Return the play predicted by the noisy introspection model for 'game' with
+    initial precision 'lam0' and telescope parameter 'tel'.
+
+    If 'return_steps' is `True`, returns `k,p`, where `k` is the number of
+    steps where the sequence converged, and `p` is the prediction.
+    Otherwise returns `p`.
+
+    Noisy introspection is defined as the limit of a sequence of noisy
+    responses; this function computes sequences of increasing length and then
+    stops when the sequence has converged.  The sequence is considered
+    converged when the L2 distance between two elements is less than 'thresh'.
+
+    Raises a `ValueError` if no convergence after 'max_steps' steps.
+    """
+    assert lam0 >= 0.0
+    assert tel > 1.0
+
+    def prediction(steps):
+        p = game.mixed_profile()
+        for k in xrange(steps, -1, -1):
+            lam = (tel**(-k)) * lam0
+            p = logit_br_all(p, lam)
+        return p
+    def delta(p1,p2):
+        return norm([x1-x2 for (x1,x2) in zip(p1,p2)])
+
+    K = 0
+    p1 = game.mixed_profile()
+    p2 = prediction(K)
+    while delta(p1,p2) > thresh:
+        K += 1
+        p1 = p2
+        p2 = prediction(K)
+        if max_steps is not None and K > max_steps:
+            raise ValueError("noisy_introspection: too many steps with no convergence on game %s with lam0=%s tel=%s", game.title, lam0, tel)
+
+    if return_steps:
+        return K,p2
+    else:
+        return p2
