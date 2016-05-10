4f29543bd7be42a3907b31d36a753b63055508f2
James Wright <james@chumsley.org>
2016-05-10 15:59:28 -0700

    expose normalize_activations from weighted_linear_l0_prediction

--- level0.py
+++ level0.py
@@ -7,7 +7,7 @@ from bogota.utils import action_profiles, normalize, proportionally_mix_profiles
 
 # ================================ Meta-models ================================
 
-def weighted_linear_l0_prediction(features, weights, game, applicable_only):
+def weighted_linear_l0_prediction(features, weights, game, applicable_only, normalize_activations=True):
     """
     Return a mixed profile representing a level-0 prediction for ``game``
     computed by taking a normalized weighted average of ``features`` according
@@ -16,16 +16,18 @@ def weighted_linear_l0_prediction(features, weights, game, applicable_only):
     ws = []
     fps = []
     for (w,f) in zip(weights, features):
-        fp = normalize(f(game))
+        fp = f(game)
         if '_inv' in dir(f) and f._inv:
             for i in xrange(len(fp)):
                 fp[i] = 1.0 - fp[i]
+        if normalize_activations:
             fp = normalize(fp)
         if f is constant_binary or applicable_only == False or applicable(fp):
             ws.append(w)
             fps.append(fp)
 
-    return proportionally_mix_profiles(ws, fps)
+    mixed = proportionally_mix_profiles(ws, fps)
+    return normalize(mixed)    
 
 def applicable(fp):
     """
