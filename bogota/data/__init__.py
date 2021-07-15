8b1ec3473b2d622de2bb561590e12be06c063039
Parhamglst <parhamglst@gmail.com>
2021-07-15 13:07:38 -0600

    import headers updated

--- bogota/data/__init__.py
+++ bogota/data/__init__.py
@@ -1,46 +1,46 @@
 from bogota.datapool import DataPool
 from bogota.traceset import TraceSet
 
-from . import pools.cn_some9
+from .pools import cn_some9
 cn_some9 = pools.cn_some9.cn_some9
 cn_some8 = pools.cn_some9.cn_some8
 
-from . import pools.cn_stahl1994experimental
+from .pools import cn_stahl1994experimental
 cn_stahl1994experimental = pools.cn_stahl1994experimental.cn_stahl1994experimental
 
-from . import pools.cn_stahl1995players
+from .pools import cn_stahl1995players
 cn_stahl1995players = pools.cn_stahl1995players.cn_stahl1995players
 
-from . import pools.cn_costagomes1998cognition
+from .pools import cn_costagomes1998cognition
 cn_costagomes1998cognition = pools.cn_costagomes1998cognition.cn_costagomes1998cognition
 cn_costagomes1998cognition_traces = pools.cn_costagomes1998cognition.cn_costagomes1998cognition_traces
 
-from . import pools.cn_goeree2001ten
+from .pools import cn_goeree2001ten
 cn_goeree2001ten = pools.cn_goeree2001ten.cn_goeree2001ten
 cn_gh01_treasure = pools.cn_goeree2001ten.cn_gh01_treasure
 cn_gh01_contra = pools.cn_goeree2001ten.cn_gh01_contra
 
-from . import pools.cn_haruvy2001modeling
+from .pools import cn_haruvy2001modeling
 cn_haruvy2001modeling = pools.cn_haruvy2001modeling.cn_haruvy2001modeling
 
-from . import pools.cn_cooper2003evidence
+from .pools import cn_cooper2003evidence
 cn_cooper2003evidence = pools.cn_cooper2003evidence.cn_cooper2003evidence
 
-from . import pools.cn_haruvy2007equilibrium
+from .pools import cn_haruvy2007equilibrium
 cn_haruvy2007equilibrium = pools.cn_haruvy2007equilibrium.cn_haruvy2007equilibrium
 
-from . import pools.cn_stahl2008leveln
+from .pools import cn_stahl2008leveln
 cn_stahl2008leveln = pools.cn_stahl2008leveln.cn_stahl2008leveln
 
-from . import pools.cn_camerer2007heterogeneous
+from .pools import cn_camerer2007heterogeneous
 cn_camerer2007heterogeneous = pools.cn_camerer2007heterogeneous.cn_camerer2007heterogeneous
 cn_rogers2009heterogeneous = cn_camerer2007heterogeneous # final journal version
 
-from . import pools.cn_costagomes2008stated
+from .pools import cn_costagomes2008stated
 cn_costagomes2008stated = pools.cn_costagomes2008stated.cn_costagomes2008stated
 cn_costagomes2008stated_traces = pools.cn_costagomes2008stated.cn_costagomes2008stated_traces
 
-from . import pools.cn_costagomes2006cognition
+from .pools import cn_costagomes2006cognition
 cn_costagomes2006cognition_bin10 = pools.cn_costagomes2006cognition.cn_costagomes2006cognition_bin10
 cn_costagomes2006cognition_bin10_traces = pools.cn_costagomes2006cognition.cn_costagomes2006cognition_bin10_traces
 cn_costagomes2006cognition_bin25 = pools.cn_costagomes2006cognition.cn_costagomes2006cognition_bin25
